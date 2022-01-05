732d4f05c7fb68ea86234637d7efab8f360ae1d3
Immortal215 <97135755+Immortal215@users.noreply.github.com>
2022-01-04 18:11:26 -0600

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,16 @@
+from random import randint
+guess=input("What is your guess?")
+number = randint(1,9)
+numbers=number
+
+
+
+
+if (guess == number): 
+    print("congratulations, YOU WON!!!")
+else: 
+    print("That is not the correct answer. The answer was:", numbers)
+
+
+     
+     
